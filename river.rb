0da87e4d96dfa7167988966ed1aeb22f1593102f
gcollins1317 <gracecollins1000@gmail.com>
2018-07-13 15:15:53 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -4,8 +4,8 @@ class River
     @name = name 
   end 
   def flood 
-   @discharge = discharge * 1.3 
+   @discharge = @discharge * 1.3 
   end 
   def dry_up 
-    @discharge = discharge * 0.5 
+    @discharge = @discharge * 0.5 
 end 
